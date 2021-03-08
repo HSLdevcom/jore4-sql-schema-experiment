ab67c2a9bd4848efccc899bd8ba1e24b452c583e
haphut <haphut@mistmap.com>
2021-03-08 19:57:49 +0200

    Add table infrastructure_network_types
    We need to know whether an infrastructure link describes a road segment,
    a rail segment etc. The table infrastructure_network_types acts as a
    reference table for that purpose.

--- hasura/migrations/1615209774035_infrastructure_network_types/up.sql
+++ hasura/migrations/1615209774035_infrastructure_network_types/up.sql
@@ -7,3 +7,18 @@ COMMENT ON COLUMN
 COMMENT ON COLUMN
   infrastructure_network.infrastructure_links.infrastructure_link_geog IS
   'The PostGIS geography of the infrastructure link in WGS 84.';
+
+CREATE TABLE infrastructure_network.infrastructure_network_types (
+  infrastructure_network_type_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  infrastructure_network_type_name text NOT NULL UNIQUE
+);
+
+COMMENT ON TABLE
+  infrastructure_network.infrastructure_network_types IS
+  'The types of the infrastructure network, e.g. road, rail, tram, metro. Not in Transmodel.';
+COMMENT ON COLUMN
+  infrastructure_network.infrastructure_network_types.infrastructure_network_type_id IS
+  'The ID of the infrastructure network type.';
+COMMENT ON COLUMN
+  infrastructure_network.infrastructure_network_types.infrastructure_network_type_name IS
+  'The name of the infrastructure network type, e.g. "road", "rail", "tram", "metro".';
