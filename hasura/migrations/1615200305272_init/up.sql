6436f2d58f7f1cc1b07c55b4b359514f024a3fbd
Tuomas Kaittola <tuomas.kaittola@vincit.fi>
2021-03-19 09:06:55 +0200

    Explicitly create extensions
    !! Note: This change is not backwards compatible, but that should be
    ok as currently this codebase is just for experiments.

--- hasura/migrations/1615200305272_init/up.sql
+++ hasura/migrations/1615200305272_init/up.sql
@@ -1,4 +1,11 @@
+DROP SCHEMA IF EXISTS public CASCADE; -- drop public schema as it might not be available e.g. in azure's db
+CREATE SCHEMA postgis;
+CREATE SCHEMA pgcrypto;
+CREATE EXTENSION postgis WITH SCHEMA postgis;
+CREATE EXTENSION pgcrypto WITH SCHEMA pgcrypto;
 CREATE SCHEMA infrastructure_network;
+
+SELECT set_config('search_path', 'postgis, pgcrypto,'||current_setting('search_path'), false);
 
 CREATE TABLE infrastructure_network.infrastructure_links (
   infrastructure_link_id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
